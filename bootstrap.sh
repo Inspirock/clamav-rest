2380fb61e55d0512e6d8d923592636fbc5a28ac8
Ashutosh Mimani <ashutosh@binarybuddhastudio.com>
2019-04-04 11:32:10 +0530

    change default filesize

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -m
 
 host=${CLAMD_HOST:-192.168.50.72}
 port=${CLAMD_PORT:-3310}
-filesize=${MAXSIZE:-10GB}
+filesize=${MAXSIZE:-20000KB}
 apikey=${API_KEY:-ABCDEFGHIJKLMNOPQRSTUVWXYZ}
 
 echo "using clamd server: $host:$port"
